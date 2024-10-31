8a6a3aa5db7ae4e41369d30ea47c7e9035d23108
Hafsa-Shahid-khan <yhayakhankhan202@gmail.com>
2024-10-31 15:21:25 +0500

    Add files via upload
    I make may assignment in Python V.S code.

---
+++ python 1/assignment.py
@@ -0,0 +1,41 @@
+#Question no 1
+number1=5
+number2=16
+number3=10
+
+sum=number1+number2+number3 #sum 
+print("sum:",sum)
+
+diffrence=number1-number2-number3 #diffrence
+print("sum:",sum)
+
+product=number1*number2*number3 #product
+print("product",product)
+
+average=(number1+number2+number3)/3 #average
+print("average",average)
+
+#Queation no 2
+
+#triangle area
+base=5
+height=10
+area=0.5*base*height
+print("area of triangle",area)
+
+#square area
+side=10
+area=side*side
+print("area of square",area)
+
+#circle area
+pi=3.14
+radius=20
+area=pi*radius*radius
+print("area of circle",area)
+
+#rectangle area
+length=10
+width=5
+area=length*width
+print("area of rectangle",area)
